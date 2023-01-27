b21995556dc7b3d177a624598dbfac50d4096497
Krushana-chavan <104185649+Krushana-chavan@users.noreply.github.com>
2023-01-27 13:37:35 +0530

    last deploy

--- index.js
+++ index.js
@@ -8,4 +8,35 @@ app.use(express.json())
 
 app.get('/',(req,res)=> res.send('hello'))
 
+
+
+
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+app.get('/random-six', (req, res) => {
+    let randomWord = "";
+    for (let i = 0; i < 6; i++) {
+        randomWord += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    res.send(randomWord);
+});
+app.get('/random-four', (req, res) => {
+    let randomWord = "";
+    for (let i = 0; i < 4; i++) {
+        randomWord += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    res.send(randomWord);
+});
+
+
+
+
+
+
 app.listen(8080,()=>{  console.log('server statrted on port 8080')})
+
+// define a list of words
+
+
+// start the server
+
