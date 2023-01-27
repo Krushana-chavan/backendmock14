d0a535df89c333150af38cc003463249d6f37780
Krushana-chavan <104185649+Krushana-chavan@users.noreply.github.com>
2023-01-27 13:49:03 +0530

    last deploy

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ app.get('/',(req,res)=> res.send('hello'))
 
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-app.get('/random-six', (req, res) => {
+app.get('/random', (req, res) => {
     let randomWord = "";
-    for (let i = 0; i < 6; i++) {
+    const length = Math.floor(Math.random() * 6) + 1;
+    for (let i = 0; i < length; i++) {
         randomWord += alphabet[Math.floor(Math.random() * alphabet.length)];
     }
     res.send(randomWord);
