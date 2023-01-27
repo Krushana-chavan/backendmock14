c7071805d2966613eb0019fe02038d10cb67b0a5
Krushana-chavan <104185649+Krushana-chavan@users.noreply.github.com>
2023-01-27 14:27:20 +0530

    done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
 const app = express()
-
+const cors = require("cors")
 app.use(express.urlencoded({extended:true}))
-
+app.use(cors())
 app.use(express.json())
 
 app.get('/',(req,res)=> res.send('hello'))
